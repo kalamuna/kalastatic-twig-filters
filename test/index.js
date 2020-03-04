45e9e1174b98d5e369e98a7272e8d11febdaf4f2
Rob Loach <robloach@gmail.com>
2020-03-04 18:57:05 -0500

    Add xo code checkiung

--- test/index.js
+++ test/index.js
@@ -1,81 +1,70 @@
-var assert = require('assert'),
-    twigFilters = require('..'),
-    Twig = require('twig'),
-    twig = Twig.twig;
+const assert = require('assert');
+const twigFilters = require('..');
+const Twig = require('twig');
+const twig = Twig.twig;
 
-describe('kalastatic-twig-filters', function(){
+describe('kalastatic-twig-filters', () => {
   // Add the Twig Filters to Twig.
   twigFilters(Twig);
 
-  it('should slugify string', function(done){
-
-    var template = twig({
-      data: "{{ value|slug }}"
+  it('should slugify string', done => {
+    const template = twig({
+      data: '{{ value|slug }}'
     });
 
-    var output = template.render({value: "This is only a Test!"});
+    const output = template.render({value: 'This is only a Test!'});
 
-    assert(output === "this-is-only-a-test");
+    assert(output === 'this-is-only-a-test');
 
     done();
-
   });
 
-  it('should do a slug match on a page title string', function(done){
-
-    var template = twig({
-      data: "{{ value|isSlugMatch('About Us') }}"
+  it('should do a slug match on a page title string', done => {
+    const template = twig({
+      data: '{{ value|isSlugMatch(\'About Us\') }}'
     });
-    var output = template.render({value: "about-us"});
-    assert( Boolean(output) === true);
+    const output = template.render({value: 'about-us'});
+    assert(Boolean(output) === true);
 
     done();
-
   });
 
-  it('should truncate a string and append an ellipsis on a full word', function(done){
-
-    var template = twig({
-      data: "{{ value|limit(24) }}"
+  it('should truncate a string and append an ellipsis on a full word', done => {
+    const template = twig({
+      data: '{{ value|limit(24) }}'
     });
-    var output = template.render({value: "One two three four five six"});
-    assert( output === "One two three four five…" );
+    const output = template.render({value: 'One two three four five six'});
+    assert(output === 'One two three four five…');
 
     done();
-
   });
 
-  it('should set up the phone numbers correctly', function(done){
-
-    var template = twig({
-      data: "{{ value|phone }}"
+  it('should set up the phone numbers correctly', done => {
+    const template = twig({
+      data: '{{ value|phone }}'
     });
-    var output = template.render({value: "925-255-5204"});
+    const output = template.render({value: '925-255-5204'});
     assert.equal(output, '(925) 255-5204');
 
     done();
-
   });
 
-  it('should set up the phone numbers according to the given format', function(done){
-
-    var template = twig({
-      data: "{{ value|phone('NNN.NNN.NNNN') }}"
+  it('should set up the phone numbers according to the given format', done => {
+    const template = twig({
+      data: '{{ value|phone(\'NNN.NNN.NNNN\') }}'
     });
-    var output = template.render({value: "+19252555204"});
+    const output = template.render({value: '+19252555204'});
     assert.equal(output, '925.255.5204');
 
     done();
-
   });
 
-  it('should append a question mark and a 10 digit numeric string based on date.now() eg: "?1467994846"', function(done){
-    var template = twig({
-      data: "{{ value|bustcache }}"
+  it('should append a question mark and a 10 digit numeric string based on date.now() eg: "?1467994846"', done => {
+    const template = twig({
+      data: '{{ value|bustcache }}'
     });
-    var output = template.render({value: "styles.css"});
+    const output = template.render({value: 'styles.css'});
     assert.equal(output.indexOf('?'), 10);
     done();
   });
-
 });
